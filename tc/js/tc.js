3e5f51d769a07326e80aefaa5b0a6ca1ba13afe4
FTXG <3033472448@qq.com>
2023-11-06 15:56:04 +0800

    背景更换

--- tc/js/tc.js
+++ tc/js/tc.js
@@ -1,4 +1,14 @@
+var countNum = 0;
 $(function () {
+    var interID = undefined;
+    $(document).on("mousedown",function (e) {
+        interID = countInterval();
+    }).on("mouseup",function (e) {
+        if (interID != undefined && interID != null){
+            clearInterval(interID);
+            countNum = 1;
+        }
+    })
     //头部时间
     setInterval(function () {
         var hourNum =  DateUtil.formatDateTime(new Date(),"HH");
@@ -9,3 +19,16 @@ $(function () {
         }
     },1000)
 })
+//计数器
+function countInterval() {
+    numsID = setInterval(function () {
+        countNum ++;
+    },500)
+    return numsID;
+}
+//点击光标
+function cursor(countNum) {
+    $(document).on("mousemove",function (e) {
+
+    })
+}
