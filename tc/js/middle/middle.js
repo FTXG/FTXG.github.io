3b4ad7575dcbb5842c3465136e3a0e2885fc2f35
FTXG <3033472448@qq.com>
2023-12-12 12:05:35 +0800

    一言

--- tc/js/middle/middle.js
+++ tc/js/middle/middle.js
@@ -1,11 +1,5 @@
 $(function () {
     $(".middle-img60s").on('click',function () {
-        // // 获取当前点击小图的src
-        // let imgSrc = $(this).attr("src");
-        // // 将获取到的src赋值到大图显示区域img的src里
-        // $(".big_img").attr("src", imgSrc);
-        // // 显示大图区域
-        // $(".big").show();
         // 获取当前点击小图的src
         let imgObj = $(this).clone();
         imgObj.attr("class","big_img");
