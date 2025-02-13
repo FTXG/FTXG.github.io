c340e9e5740150b93c22fc24fa78b8dfd2adc8db
FTXG <3033472448@qq.com>
2025-02-13 10:59:06 +0800

    删除部分代码

--- video/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#boxTop").on('mouseover','.qin',function (e) {
-        $(this).animate({width:'160px'},'50')
-    }).on('mouseout','.qin',function (e) {
-        $(this).animate().stop(true);
-        // $(this).css({'width':'40px'});
-        $(this).animate({width:'40px'},'10')
-    })
-})
